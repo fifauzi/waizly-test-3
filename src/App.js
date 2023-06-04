2ee754c5bffb610244dd29e8f0a1a43f7bcfb2bd
unknown <fifauzi24@gmail.com>
2023-06-04 12:28:17 +0700

    No3 Waizly test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TimeConversion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time12Hour: '',
+      time24Hour: ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ time12Hour: event.target.value });
+  };
+
+  convertTime = () => {
+    const { time12Hour } = this.state;
+    
+    // Extract the hour, minute, and second components
+    let [hour, minute, second] = time12Hour.split(':');
+
+    // Convert hour to integer
+    hour = parseInt(hour);
+
+    // Check if it's PM and not 12:00:00 PM
+    if (time12Hour.includes('PM') && hour !== 12) {
+      hour += 12;
+    }
+    // Check if it's AM and 12:00:00 AM
+    else if (time12Hour.includes('AM') && hour === 12) {
+      hour = 0;
+    }
+
+    // Format the time in 24-hour military format
+    const time24Hour = `${hour}:${minute}:${second}`;
+    this.setState({ time24Hour });
+  };
+
+  render() {
+    const { time12Hour, time24Hour } = this.state;
+
+    return (
+      <div>
+        <input
+          type="text"
+          value={time12Hour}
+          onChange={this.handleInputChange}
+          placeholder="Enter time in 12-hour format"
+        />
+        <button onClick={this.convertTime}>Convert</button>
+        <p>Time in 24-hour format: {time24Hour}</p>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TimeConversion;
